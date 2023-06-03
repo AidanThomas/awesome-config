7c25d23bd8cdba03f098f521799297dadd72ecc8
Aidan Thomas <aidant@agylia.com>
2023-06-03 18:12:11 +0100

    Update position monitor script to set refresh rate

--- position_monitors.sh
+++ position_monitors.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-xrandr --output DP-4 --auto
-xrandr --output DP-2 --primary --right-of DP-4
-xrandr --output DP-0 --primary --right-of DP-2
+xrandr --output DP-4 --mode 2560x1440 --rate 143.97 --auto
+xrandr --output DP-2 --mode 2560x1440 --rate 143.97 --primary --right-of DP-4
+xrandr --output DP-0 --mode 2560x1440 --rate 143.97 --primary --right-of DP-2
